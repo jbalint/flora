19feeacd0c345b5666a29cd8009f16865bcf77a8
kifer <kifer@0877669f-6caa-400d-a9c2-f8e882ed57a8>
2011-07-29 07:04:45 +0000

    on the way to 64 bit
    git-svn-id: svn://svn.code.sf.net/p/flora/src/trunk@361 0877669f-6caa-400d-a9c2-f8e882ed57a8

--- silk/flora2/makeflora.bat
+++ silk/flora2/makeflora.bat
@@ -25,14 +25,15 @@ if "%1" == "-I" shift
 if "%1" == "-S" echo #define FLORA_SUBSUMPTIVE_TABLING > %default_tabling%
 if "%1" == "-S" shift
 
-if "%1" == "" echo Usage:  makeflora [-c] full-path-to-prolog
+if "%1" == "" echo Usage:  makeflora [-c|-c64] full-path-to-prolog
 if "%1" == "" goto end
 
-if "%1" == "-c"  set PROLOG=%2 -s -m 40000 -c 4000
-if not "%1" == "-c"  set PROLOG=%1 -s -m 40000 -c 4000
+if        "%1" == "-c"    (set PROLOG=%2 -s -m 40000 -c 4000
+) else if "%1" == "-c64"  (set PROLOG=%2 -s -m 40000 -c 4000
+) else                    set PROLOG=%1 -s -m 40000 -c 4000
 
 @echo.
-if "%PROLOG%" == "" echo Usage:  makeflora [-c] full-path-to-prolog
+if "%PROLOG%" == "" echo Usage:  makeflora [-c|-c64] full-path-to-prolog
 if "%PROLOG%" == "" goto end
 
 if "%1" == "clean" if not exist binary-distribution.txt nmake /f NMakefile.mak clean
@@ -47,14 +48,19 @@ if exist runflora.bat  del runflora.bat
 call %PROLOG% -e "[flrconfig]. halt."
 
 cd p2h
-del prolog2hilog.dll prolog2hilog.lib prolog2hilog.def prolog2hilog.exp
-del prolog2hilog.obj prolog2hilog.a prolog2hilog.o prolog2hilog.xwam
-echo ***The above `Could Not Find prolog2hilog.*' messages are NORMAL
+if exist prolog2hilog.dll   del prolog2hilog.dll
+if exist prolog2hilog.lib   del prolog2hilog.lib
+if exist prolog2hilog.def   del prolog2hilog.def
+if exist prolog2hilog.exp   del prolog2hilog.exp
+if exist prolog2hilog.obj   del prolog2hilog.obj
+if exist prolog2hilog.a     del prolog2hilog.a
+if exist prolog2hilog.o     del prolog2hilog.o
+if exist prolog2hilog.xwam  del prolog2hilog.xwam
 
-if "%1" == "-c" if not exist ..\binary-distribution.txt nmake /f NMakefile.mak
-if "%1" == "-c" if not exist ..\binary-distribution.txt del prolog2hilog.obj
+if "%1" == "-c"   if not exist ..\binary-distribution.txt nmake /f NMakefile.mak
+if "%1" == "-c"   if not exist ..\binary-distribution.txt if exist prolog2hilog.obj del prolog2hilog.obj
 if "%1" == "-c64" if not exist ..\binary-distribution.txt nmake /f NMakefile64.mak
-if "%1" == "-c64" if not exist ..\binary-distribution.txt del prolog2hilog.obj
+if "%1" == "-c64" if not exist ..\binary-distribution.txt if exist prolog2hilog.obj del prolog2hilog.obj
 
 cd ..
 
