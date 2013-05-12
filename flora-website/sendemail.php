04c444cce8f72a5b4db81a725dbdf5b193ac828b
kifer <kifer@0877669f-6caa-400d-a9c2-f8e882ed57a8>
2013-05-12 04:42:42 +0000

    added website
    git-svn-id: svn://svn.code.sf.net/p/flora/src/trunk@872 0877669f-6caa-400d-a9c2-f8e882ed57a8

--- flora-website/sendemail.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-	$to = "flora-users@lists.sourceforge.net";
-	if (mail($to, $subject, $body, ("From: " . $address))) {
-	   printf("<br>Email has been sent. Thank you.");
-	} else {
-	   printf("<br>Sorry, an error occurred while sending email. Please try later.");
-	}
-?>
--- flora-website/tutorial/Tarball.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh 
-
-
-cd ..
-
-EXCLUDEFILE=tutorial/.excludedFiles
-
-tar cvf tutorial/tutorial.tar --exclude-from=$EXCLUDEFILE tutorial
-
